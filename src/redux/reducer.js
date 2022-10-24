cbae45092b05727cfddc1f3e52cd90139cc88baf
Peter <peteryurich@gmail.com>
2022-10-24 12:10:14 +0300

    com

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,11 +1,11 @@
 const contactsInitialState = [];
 
-// export const tasksReducer = (state = contactsInitialState, action) => {
-//     switch (action.type) {
-//         case "contacts/addTask":
-//             return ''
-//     }
-// }
+export const tasksReducer = (state = contactsInitialState, action) => {
+    switch (action.type) {
+        case "contacts/addContact":
+            return [ ...state, action.payload];
+    }
+}
 
 export const rootReducer = () => {
     return ''
